f96ed2f2640557b9c2b58d9435ac8a26cd4213fb
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-07 10:07:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-        "vj chatgpt bot",
+        "vj join request bot",
          api_id=API_ID,
          api_hash=API_HASH,
          bot_token=BOT_TOKEN,
