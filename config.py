abd00ce1d55ee546c72e4b56d4265bd1817ce121
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-07 10:08:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
-import re
-from os import getenv, environ
-
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
+from os import environ
 
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
@@ -16,6 +6,4 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 ADMINS = int(environ.get("ADMINS", ""))
 DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
-OPENAI_API = environ.get("OPENAI_API", "")
-AI = is_enabled((environ.get("AI","True")), False)
+DB_NAME = environ.get("DB_NAME", "vjjoinrequestbot")
