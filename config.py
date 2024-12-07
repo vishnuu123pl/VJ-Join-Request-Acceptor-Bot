1da3e6adeeb52e6fe373f1e830edc2f4fceb719e
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-07 11:22:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6627318053:AAHsVWm9XY3jU0XeAfWoFB_eKOasyzFhz9g")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002131110143"))
+ADMINS = int(environ.get("ADMINS", "6073523936"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://mihaja5084:yeIh95RrMkRNZ3It@cluster0.6voc3fm.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "vjjoinrequestbot")
