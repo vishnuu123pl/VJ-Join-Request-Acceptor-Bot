14a76773f1a842235559a9f2124a24131c547a8b
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-01-09 16:56:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 ADMINS = int(environ.get("ADMINS", ""))
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+
+# If this is True Then Bot Accept New Join Request 
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
